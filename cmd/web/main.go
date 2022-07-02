32658a86024b811a38fb20dcac7ab6af163381a8
Ankur Tiwari <ankurtiwari74@yahoo.com>
2022-07-02 19:06:17 +0530

    deploy on AWS

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -50,9 +50,9 @@ func main() {
 		Handler: routes(&app),
 	}
 
-	// err = srv.ListenAndServe()
-	// log.Fatal(err)
-	srv.Close()
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+	// srv.Close()
 }
 
 func run() (*driver.DB, error) {
